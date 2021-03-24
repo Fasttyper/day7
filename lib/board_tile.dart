dd76a1b327fbddfa8acedf59576b84ac714cb146
Bakhtiyorjon Bokhodirov <bakhtiyorjonbokhodirov@gmail.com>
2021-03-24 19:43:09 +0300

    Day 1

---
+++ lib/board_tile.dart
@@ -0,0 +1,45 @@
+import 'package:day7/tile_state.dart';
+import 'package:flutter/material.dart';
+
+class BoardTile extends StatelessWidget{
+
+  final double dimension;
+
+  final VoidCallback onPressed;
+
+  final TileState tileState;
+
+  BoardTile({Key key, this.tileState, this.dimension, this.onPressed,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      width: dimension,
+      height: dimension,
+      //color: Colors.blue,
+      child: IconButton(
+        onPressed: onPressed,
+        icon: _widgetForTileState(),
+      ),
+    );
+  }
+
+  Widget _widgetForTileState(){
+
+    Widget widget;
+
+    switch (tileState){
+      case TileState.EMPTY: {
+        widget = Image.asset(Icons.hourglass_empty);
+      } break;
+      case TileState.CROSS: {
+        widget = Image.asset("images/x.png");
+      } break;
+      case TileState.CIRCLE: {
+        widget = Image.asset("images/o.png");
+      } break;
+    }
+
+    return widget;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:day7/board_tile.dart';
+import 'package:day7/tile_state.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,8 +16,53 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-         
+        body: Center(
+          child: Stack(
+            children: [
+              Image.asset('images/board.png'),
+              _boardTiles(),
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  Widget _boardTiles() {
+    return Builder(builder: (context) {
+      final boardDimension = MediaQuery.of(context).size.width;
+
+      final tileDimension = boardDimension / 3;
+
+      return Container(
+        width: boardDimension,
+        height: boardDimension,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                BoardTile(dimension: tileDimension, tileState: TileState.CROSS,),
+                BoardTile(dimension: tileDimension, tileState: TileState.CROSS,),
+                BoardTile(dimension: tileDimension, tileState: TileState.CROSS,),
+              ],
+            ),
+            Row(
+              children: [
+                BoardTile(dimension: tileDimension, tileState: TileState.CROSS,),
+                BoardTile(dimension: tileDimension, tileState: TileState.CROSS,),
+                BoardTile(dimension: tileDimension, tileState: TileState.CROSS,),
+              ],
+            ),
+            Row(
+              children: [
+                BoardTile(dimension: tileDimension, tileState: TileState.CROSS,),
+                BoardTile(dimension: tileDimension, tileState: TileState.CROSS,),
+                BoardTile(dimension: tileDimension, tileState: TileState.CROSS,),
+              ],
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
